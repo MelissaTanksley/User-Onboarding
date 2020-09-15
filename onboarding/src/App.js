dfc3b5ba70687cf5cd5e5644b036df2b74d3f7d4
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-15 13:50:45 -0700

    App and Card started

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Form from './Components/Form';
+import List from './Components/List';
+import users from './users'
 
 function App() {
+  const [post, setPost] = useState(users)
+  console.log("App -> post", post)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form setPost={setPost} post={post} />
+      <List post={post} />
     </div>
   );
 }
