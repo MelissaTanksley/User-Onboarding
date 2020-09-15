aa6a50b89b70d8b3d9de2492cd9e2a427cbde3c1
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-15 14:10:04 -0700

    changes to Form

---
+++ onboarding/src/Components/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+      name: 'Melissa Tanksley',
+      password: '1234abcd',
+      email: 'melissa-tanksley@lambdastudents.com',
+      jobs: 'Registered Nurse',
+      tos: true
+    },
+    {
+      name: 'John Jacob Jingleheimer Schmidt',
+      password: '14243546kfld',
+      email: 'j_jacob@email.com',
+      jobs: 'Vendor',
+      tos: true
+    }
+  ]
+  
+  export default users 
