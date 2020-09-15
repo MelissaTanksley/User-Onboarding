b6e4a82a7194facbd3c27af3fbf14829ee15cea2
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-15 14:45:20 -0700

    update to Forms; MVP

--- onboarding/src/Components/Form.jsx
+++ onboarding/src/Components/Form.jsx
@@ -179,9 +179,8 @@ export default function Form({ setPost, post }) {
           <Alert color="warning">{errors.tos}</Alert>) : null
         }
         </FormGroup>
-        <Button color="primary" type="submit" style={{ marginTop: '20px', width: '110px' }} data-cy="submit">Sign Up</Button>
-
-              
+        <Button color="primary" type="submit" style={{ marginTop: '20px', width: '110px' }} data-cy="submit"
+        disabled={isButtonDisabled}>Sign Up</Button>
         {/* disabled={isButtonDisabled}  */}
 
     </ReactForm>
